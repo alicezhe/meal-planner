345ad7ecdc71af87b3fc340d5d4febba2362b968
alicezhe <healice101@gmail.com>
2022-03-31 12:07:14 -0400

    recipe step by step

--- frontend/src/components/RecipePage.js
+++ frontend/src/components/RecipePage.js
@@ -10,14 +10,16 @@ const RecipePage = () => {
   const [recipe, setRecipe] = useState({})
   const [loggedIn, setLoggedIn] = useState(false)
 
-  const apiKey = 'e15639e1fce043b6a7cefa240347eebc'
+  const apiKey = '3f220cadbc3646659ca213813545c978'
 
   useEffect(() => {
     const loadRecipe = async () => {
       const { data } = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${apiKey}`)
       const kcal = data.nutrition.nutrients[0].amount
-      const { title, image, summary, readyInMinutes: time} = data
-      setRecipe({ title, image, summary: parse(summary), time, kcal })
+      console.log(data)
+      let { title, image, summary, readyInMinutes: time, extendedIngredients, analyzedInstructions} = data
+      analyzedInstructions = analyzedInstructions ? analyzedInstructions[0].steps : []
+      setRecipe({ title, image, summary: parse(summary), time, kcal, extendedIngredients, analyzedInstructions })
     }
     loadRecipe()
   }, [])
@@ -33,11 +35,50 @@ const RecipePage = () => {
     return () => clearInterval(intervalID)
   }, [])
 
+  const viewRecipe = () => {
+    // recipe.analyzedInstructions.steps.map(step => console.log(step.step))
+    recipe.analyzedInstructions.map(step => console.log(step.step))
+  }
+
   return (
     <>
-      <div className="flex flex-col justify-between w-full h-full bg-light-gray rounded-3xl p-12">
+      <div className="flex flex-col justify-start w-full h-full bg-light-gray rounded-3xl p-12">
         <Navbar loggedIn={loggedIn} page="recipe" title={recipe.title} />
-         {recipe.image} {recipe.time} {recipe.kcal}
+        <div className="overflow-y-scroll scroll-div">
+          <div className="grid grid-cols-none md:grid-cols-3 my-4">
+            <div className="md:col-span-1 w-full h-[250px]">
+              <div className={`w-full h-full rounded-2xl bg-[url('${recipe.image}')] bg-no-repeat bg-center bg-cover`}></div>
+            </div>
+            <div className="md:col-span-2 mt-8 md:mt-0 md:ml-8">
+              {recipe.summary}
+            </div>
+          </div>
+          <div className="grid grid-cols-none md:grid-cols-3">
+            <div className="md:col-span-1">
+              <h3 className="text-red text-xl font-bold">Ingredients</h3>
+              <div>
+                <ul style={{columns: 2}}>
+                  {recipe.extendedIngredients?.map(ingredient => {
+                    if (ingredient.id && ingredient.original) return <li key={ingredient.id} className="my-2">{ingredient.original}</li>
+                  })}
+                </ul>
+              </div>
+            </div>
+            <div className="md:col-span-2">
+              <div className="md:ml-8">
+                <h2 className="font-bold text-red text-xl">Recipe</h2>
+                <ol>
+                  {recipe.analyzedInstructions?.map(step => {
+                    if (step.number && step.step) {
+                      return <li key={step.number} className="my-2"><p>&#8226; {step.step}</p></li>
+                    }
+                  })}
+                </ol>
+              </div>
+            </div>
+            <button onClick={viewRecipe}>click</button>
+          </div>
+        </div>
       </div>
     </>
   )
